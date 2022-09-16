b90e0e417173bd1dfb6e13ebda77312f914fc952
TaiDuongRepo <taiduong1301@gmail.com>
2022-09-16 10:26:57 +0700

    Update Quick Sort

--- LyThuyet/4_Sort/Quick_Sort/QuickSort.cpp
+++ LyThuyet/4_Sort/Quick_Sort/QuickSort.cpp
@@ -5,15 +5,24 @@ void QuickSort(int a[], int left, int right);
 
 int main(int argc, char const *argv[])
 {
+    int n = 8;
+    int a[] = {3, 6, 4, 7, 2, 1, 8, 9};
+    QuickSort(a, 0, 8);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
     
+
     return 0;
 }
 
 void QuickSort(int a[], int left, int right) {
     int i, j, x;
-    if (left > right) {
+    if (left > right)
     {
-        //return;
+        return;
     }
     x = a[(left + right)/2];
     i = left;
